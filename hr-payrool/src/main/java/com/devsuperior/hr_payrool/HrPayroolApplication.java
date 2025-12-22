34ddb2152408ebd6231e926b3fcf383771bd9b76
Jefferson Lucas <jefferson@dimensiva.com.br>
2025-12-22 17:41:43 -0300

    chore improvement hr wortker

--- hr-payrool/src/main/java/com/devsuperior/hr_payrool/HrPayroolApplication.java
+++ hr-payrool/src/main/java/com/devsuperior/hr_payrool/HrPayroolApplication.java
@@ -2,12 +2,14 @@ package com.devsuperior.hr_payrool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @RibbonClient(name = "hr-worker")
+@EnableDiscoveryClient
 public class HrPayroolApplication {
 
 	public static void main(String[] args) {
